07fbfc9f04611f2d5b4e3a2bc02bce3a16ee6c77
Koleman <kolemanmark4@gmail.com>
2026-06-01 05:46:54 +0100

    Fix tsconfig and next config

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,5 +1,10 @@
 /** @type {import('next').NextConfig} */
+const path = require('path');
 const nextConfig = {
   images: { remotePatterns: [{ protocol: 'https', hostname: '**' }] },
+  webpack: (config) => {
+    config.resolve.alias['@'] = path.resolve(__dirname, 'src');
+    return config;
+  },
 };
 module.exports = nextConfig;
